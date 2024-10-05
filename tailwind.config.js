86f16869261174e142291d75d769e3b01fff539c
Ngo Manh Thong <ngomanhthong0812>
2024-10-05 20:00:55 +0700

    update checout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        '3xl': '1920px', // Bạn có thể chỉnh sửa giá trị này để phù hợp với kích thước màn hình lớn hơn.
+        '3xl': '1920px',
       },
     },
   },
